a1e6a39ea5ecef18c291cbc1355bedfc3ea7562e
danilkinkin <danilkinkin@gmail.com>
2022-10-31 22:37:45 +0400

    init: initialize infrastructure

---
+++ src/home.tsx
@@ -0,0 +1,11 @@
+import { For, createSignal } from 'solid-js';
+
+type Todo = { id: number, text: string, completed: boolean };
+
+export const Home = () => {
+  return (
+    <>
+      <h1>Buckwheat</h1>
+    </>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import { render } from 'solid-js/web';
+
+import { Home } from './home';
+
+render(() => <Home />, document.getElementById('root') as HTMLElement);
