978845980c3e88ae49819ad75c540375fdcb5459
danilkinkin <danilkinkin@gmail.com>
2023-03-01 00:33:29 +0400

    feat: added fill form tip

--- modules/contribute/donateToFundsCard.tsx
+++ modules/contribute/donateToFundsCard.tsx
@@ -1,26 +1,41 @@
-import styles from '@/modules/contribute/ruFunds.module.css';
+import styles from '@/modules/contribute/donateToFundsCard.module.scss';
 import { Card, CardLink } from '@/components/card';
 import clsx from 'clsx';
+import Link from 'next/link';
+import InfoIcon from '@/assets/icons/info.svg';
 
 export default function DonateToFunsCard() {
   return (
-    <Card
-      className={styles.card}
-      title="Пожертвуйте в фонды"
-      subtitle="Сейчас с России есть много людей, которые остались наедине с системой и очень нуждаются в помощи. Рассмотрите возможность пожертвовать в разные фонды помощи. Им эта помощь нужна больше чем мне. Вот несколько фондов которые я бы посоветовал"
-    >
-      <CardLink
-        className={clsx(styles.link, styles.topOffset)}
-        title="Независимый правозащитный проект"
-        hrefTitle="ovdinfo.org"
-        href="https://ovdinfo.org/"
-      />
-      <CardLink
-        className={styles.link}
-        title="Фонд помощи социально-незащищённым гражданам"
-        hrefTitle="nuzhnapomosh.ru"
-        href="https://nuzhnapomosh.ru/"
-      />
-    </Card>
+    <div className={styles.cardContainer}>
+      <Card
+        className={styles.card}
+        title="Пожертвуйте в фонды"
+        subtitle="Сейчас с России есть много людей, которые остались наедине с системой и очень нуждаются в помощи. Рассмотрите возможность пожертвовать в разные фонды помощи. Им эта помощь нужна больше чем мне. Вот несколько фондов которые я бы посоветовал"
+      >
+        <CardLink
+          className={clsx(styles.link, styles.topOffset)}
+          title="Независимый правозащитный проект"
+          hrefTitle="ovdinfo.org"
+          href="https://ovdinfo.org/"
+        />
+        <CardLink
+          className={styles.link}
+          title="Фонд помощи социально-незащищённым гражданам"
+          hrefTitle="nuzhnapomosh.ru"
+          href="https://nuzhnapomosh.ru/"
+        />
+      </Card>
+      <p className={styles.captionLink}>
+        <InfoIcon />
+        Если вы решились на пожертвование, заполните, пожалуйста,{' '}
+        <Link
+          href="https://docs.google.com/forms/d/e/1FAIpQLSearMo38RGIMXN4DOuLRLKRRl4O43gKPB9Mvpc3on6rEYJKow/viewform"
+          target="_blank"
+        >
+          короткую форму
+        </Link>
+        . Она анонимная, просто хочу понять сколько людей я сподвиг
+      </p>
+    </div>
   );
 }
--- modules/contribute/helpUkraineCard.tsx
+++ modules/contribute/helpUkraineCard.tsx
@@ -1,29 +1,43 @@
-import styles from '@/modules/contribute/ukraine.module.css';
+import styles from '@/modules/contribute/helpUkraineCard.module.scss';
 import { Card, CardLink } from '@/components/card';
 import clsx from 'clsx';
 import UkraineMapSvg from '@/assets/images/ukraine-map.svg';
-import Image from 'next/image';
+import Link from 'next/link';
+import InfoIcon from '@/assets/icons/info.svg';
 
 export default function HelpUkraineCard() {
   return (
-    <Card
-      className={styles.card}
-      title="Support Ukraine"
-      subtitle="For more than a year, the Russian authorities have been destroying a peaceful state. These people need help to defend their freedom."
-    >
-      <CardLink
-        className={clsx(styles.link, styles.topOffset)}
-        title="Official website of Ukraine"
-        hrefTitle="war.ukraine.ua"
-        href="https://war.ukraine.ua/"
-      />
-      <CardLink
-        className={styles.link}
-        title="Community of Ukrainian and international activists"
-        hrefTitle="supportukrainenow.org"
-        href="https://supportukrainenow.org/"
-      />
-      <UkraineMapSvg className={styles.ukraineMap} />
-    </Card>
+    <div className={styles.cardContainer}>
+      <Card
+        className={styles.card}
+        title="Support Ukraine"
+        subtitle="For more than a year, the Russian authorities have been destroying a peaceful state. These people need help to defend their freedom."
+      >
+        <CardLink
+          className={clsx(styles.link, styles.topOffset)}
+          title="Official website of Ukraine"
+          hrefTitle="war.ukraine.ua"
+          href="https://war.ukraine.ua/"
+        />
+        <CardLink
+          className={styles.link}
+          title="Community of Ukrainian and international activists"
+          hrefTitle="supportukrainenow.org"
+          href="https://supportukrainenow.org/"
+        />
+        <UkraineMapSvg className={styles.ukraineMap} />
+      </Card>
+      <p className={styles.captionLink}>
+        <InfoIcon />
+        If you decide to donate, please fill out a{' '}
+        <Link
+          href="https://docs.google.com/forms/d/e/1FAIpQLSejBz_dbR15jJ33bAwxt-gKimhnX1VzqpDZJLADYubBR7bVbg/viewform"
+          target="_blank"
+        >
+          short form
+        </Link>
+        . She is anonymous, I just want to understand how many people I inspired
+      </p>
+    </div>
   );
 }
