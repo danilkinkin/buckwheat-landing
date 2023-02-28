47a6461410dc91064ff5638734fb79ed692a950e
danilkinkin <danilkinkin@gmail.com>
2023-03-01 01:06:25 +0400

    fix: fixed locales

---
+++ modules/contribute/locales.ts
@@ -0,0 +1,25 @@
+import { LocalesMap } from '@/utils/useLocale';
+
+export const locales: LocalesMap = {
+  ru: {
+    pageTitle: 'Гречка - Вклад',
+    pageDescription:
+      'Гречка — Приложение, которое поможет вам тратить деньги с умом',
+    appName: 'Гречка',
+    description:
+      '— хобби проект и я не планирую на нём зарабатывать. У меня есть работа и постоянный заработок. Если вы очень хотите помочь, то вот несколько вариантов, что вы можете сделать. Спасибо',
+    tellFriendsTitle: 'Расскажите друзьям о приложении',
+    tellFriendsDescription:
+      'Я очень люблю когда мой проекты приносят пользу. Я был бы очень рад если вы рассказали об этом приложении друзьям',
+  },
+  en: {
+    pageTitle: 'Buckwheat - Contribute',
+    pageDescription: 'Buckwheat — App that helps you spend money wisely',
+    appName: 'Buckwheat',
+    description:
+      'is a hobby project and I do not plan to make money on it. I have a job and a steady income. If you really want to help, then here are a few things you can do. Thank you',
+    tellFriendsTitle: 'Tell your friends about the application',
+    tellFriendsDescription:
+      'I really love when my projects are useful. I would be very happy if you told your friends about this application',
+  },
+};
--- modules/home/locales.tsx
+++ modules/home/locales.tsx
@@ -2,19 +2,12 @@ import { LocalesMap } from '@/utils/useLocale';
 
 export const locales: LocalesMap = {
   ru: {
-    title: 'Гречка',
-    
-    somethingElseTitle: 'Что-то еще?',
-    somethingElseDescription:
-      'Нет, это все. Все очень просто. Гречка — это самое простое приложение, которое, как известно, может вам помочь',
-    
+    pageTitle: 'Гречка',
+    pageDescription:
+      'Гречка — Приложение, которое поможет вам тратить деньги с умом',
   },
   en: {
-    title: 'Buckwheat',
-    
-    somethingElseTitle: 'Something else?',
-    somethingElseDescription:
-      'No, it `s all. Everything is very simple. Buckwheat is the simplest application recognized to help you',
-    
+    pageTitle: 'Buckwheat',
+    pageDescription: 'Buckwheat — App that helps you spend money wisely',
   },
 };
