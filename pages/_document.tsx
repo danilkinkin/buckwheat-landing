96772ca746d827a575fa68cb663f17bfd4132478
danilkinkin <danilkinkin@gmail.com>
2023-03-01 22:24:53 +0400

    feat: added theme color meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,16 @@ export default function Document() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" type="image/ico" href="/favicon.svg" />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="#FA5A00"
+        />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: dark)"
+          content="#000"
+        />
       </Head>
       <body>
         <Main />
