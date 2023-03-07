e73673009830ab85037731fbe8d5fa09c3b23cff
danilkinkin <danilkinkin@gmail.com>
2023-03-07 15:29:53 +0400

    fix: replace favicon from `svg` to `ico` format

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,7 +23,7 @@ export default function Document(props: DocumentProps) {
     <Html>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" type="image/ico" href="/favicon.svg" />
+        <link rel="shortcut icon" type="image/ico" href="/favicon.ico" />
         <meta
           name="theme-color"
           media="(prefers-color-scheme: light)"
