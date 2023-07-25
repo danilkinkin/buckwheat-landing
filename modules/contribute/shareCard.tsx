f53431d5a08f545ddc654594375077be3dd53638
danilkinkin <d.zahvatkin@ticketscloud.com>
2023-07-25 21:25:22 +0200

    feat: added share on click share card

--- modules/contribute/shareCard.tsx
+++ modules/contribute/shareCard.tsx
@@ -8,11 +8,15 @@ import useLocale, { LocalesMap } from '@/utils/useLocale';
 
 const locales: LocalesMap = {
   ru: {
+    appName: 'Гречка',
+    slogan: 'Приложение, которое поможет вам тратить деньги с умом',
     title: 'Расскажите друзьям о приложении',
     description:
       'Мне очень важно, чтобы мои проекты приносили пользу. Если вы расскажете своим друзьям о своем опыте использования этого приложения и как оно помогло вам, я буду вам очень признателен.',
   },
   en: {
+    appName: 'Buckwheat',
+    slogan: 'App that helps you spend money wisely',
     title: 'Tell your friends about the application',
     description:
       'It is very important to me that my projects bring benefits. If you tell your friends about your experience using this application and how it helped you, I will be very grateful to you.',
@@ -29,6 +33,20 @@ export default function ShareCard() {
       subtitle={t('description')}
       backdropOnText
       classes={{ subtitle: styles.description }}
+      onClick={() => {
+        if (navigator.share) {
+          navigator
+            .share({
+              title: t('appName'),
+              text: t('slogan'),
+              url: 'https://buckwheat.app/',
+            })
+            .then(() => {
+              console.log('Thanks for sharing!');
+            })
+            .catch(console.error);
+        }
+      }}
     >
       <div className={styles.heartContiner}>
         <Image
